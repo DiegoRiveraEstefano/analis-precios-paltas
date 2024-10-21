f295b68503ff9a24e0d425f408a2b84587851290
Diego <diego.rivera.estefano@gmail.com>
2024-10-21 20:26:02 -0300

    data mining 1.0

---
+++ src/main.r
@@ -0,0 +1,5 @@
+sourceDir <- function (path, pattern = "\\.[rR]$", env = NULL, chdir = TRUE) 
+{
+    files <- sort(dir(path, pattern, full.names = TRUE))
+    lapply(files, source, chdir = chdir)
+}
